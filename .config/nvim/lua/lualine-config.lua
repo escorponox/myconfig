f8cb1cab58a2aed34754db588cf3f073970ac0a4
Carlos Coves Prieto <escorponox@gmail.com>
2022-04-28 15:23:28 +0200

    Update lualine-config.lua

--- .config/nvim/lua/lualine-config.lua
+++ .config/nvim/lua/lualine-config.lua
@@ -3,18 +3,12 @@ require'lualine'.setup {
     theme = 'gruvbox'
   },
   sections = {
-    lualine_a = {
-      {
-        'mode',
-        fmt = function(str) return str:sub(1,1) end
-      }
-    },
+    lualine_a = {},
     lualine_b = {
-      { 'branch' },
-      { 'diff', colored = true },
+      { 'diff', colored = true }
     },
+    lualine_c = { 'filename' },
     lualine_x = {
-      'filetype',
       {
         'diagnostics',
         colored = true,
@@ -26,27 +20,31 @@ require'lualine'.setup {
           warn  = { fg = 208 },  -- changes diagnostic's warn color
         },
         symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
-      },
+      }
+    }
+  },
+  tabline = {
+    lualine_a = {
+      {
+        'mode',
+        fmt = function(str) return str:sub(1,1) end
+      }
     },
-    lualine_y = { 'progress' },
+    lualine_b = {'branch'},
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      }
+    },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {
+      {
+        'tabs',
+        mode = 2, -- 0: Shows tab_nr 1: Shows tab_name 2: Shows tab_nr + tab_name
+      }
+    },
   }
 }
 
-require'tabline'.setup {
-  enable = true,
-  options = {
-    show_tabs_always = true,
-    show_filename_only = true,
-  }
-}
-
-local function setTablineShowBuffers()
-  local data = vim.fn.json_decode(vim.g.tabline_tab_data)[vim.fn.tabpagenr()]
-  if(data and data.show_all_buffers) then
-    vim.cmd('TablineToggleShowAllBuffers')
-  end
-end
-
-return {
-  setTablineShowBuffers = setTablineShowBuffers
-}
