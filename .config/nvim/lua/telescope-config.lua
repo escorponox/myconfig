e7380f64a4d4884e76844f8645f9e147852a8262
Carlos Coves Prieto <escorponox@gmail.com>
2022-04-28 15:23:58 +0200

    Update telescope-config.lua

--- .config/nvim/lua/telescope-config.lua
+++ .config/nvim/lua/telescope-config.lua
@@ -21,20 +21,12 @@ require('telescope').setup {
   },
   extensions = {
     fzf = {
-      override_generic_sorter = false, -- override the generic sorter
+      override_generic_sorter = true, -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
       case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
     },
-    -- frecency = {
-    --   db_root = "~/.local/share/nvim",
-    --   show_scores = true,
-    --   show_unindexed = true,
-    --   ignore_patterns = {"*.git/*", "*/tmp/*"},
-    --   disable_devicons = true
-    -- }
   }
 }
 
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('coc')
--- require('telescope').load_extension('frecency')
